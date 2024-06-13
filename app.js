c7c189790e7043721a7b2f2ed4df4fe85a1c22c7
Alex Merz <aleksej.merzlyakov.90@mail.ru>
2024-06-13 13:45:44 +0300

    Final Project

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const productsRouter = require('./lib/routers/products_router')
+const categoriesRouter = require('./lib/routers/categories_router')
+const usersRouter = require('./lib/routers/users_router')
+const authRouter = require('./lib/routers/auth_router')
+const profileRouter = require('./lib/routers/profile_router')
+const favoritesRouter = require('./lib/routers/favorites_router')
+const commentsRouter = require('./lib/routers/comments_router')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true,
+}))
+app.use(express.static('public'))
+
+app.use('/products', productsRouter)
+app.use('/categories', categoriesRouter)
+app.use('/users', usersRouter)
+app.use('/auth', authRouter)
+app.use('/profile', profileRouter)
+app.use('/favorites', favoritesRouter)
+app.use('/comments', commentsRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
