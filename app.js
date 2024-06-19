ca649a23e213a981b11c2afd86ecd14d57eae457
Alex Merz <aleksej.merzlyakov.90@mail.ru>
2024-06-19 20:31:27 +0300

    End

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 
 const productsRouter = require('./lib/routers/products_router')
 const categoriesRouter = require('./lib/routers/categories_router')
-const usersRouter = require('./lib/routers/users_router')
 const authRouter = require('./lib/routers/auth_router')
 const profileRouter = require('./lib/routers/profile_router')
 const favoritesRouter = require('./lib/routers/favorites_router')
@@ -20,7 +19,6 @@ app.use(express.static('public'))
 
 app.use('/products', productsRouter)
 app.use('/categories', categoriesRouter)
-app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 app.use('/profile', profileRouter)
 app.use('/favorites', favoritesRouter)
